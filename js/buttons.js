bfedc1211f3c9478411153cc5e4b12521f7fdaed
Kyle White <kylewhite0225@gmail.com>
2022-06-29 12:46:48 -0700

    Added button handling for three showcase buttons.

--- js/buttons.js
+++ js/buttons.js
@@ -15,3 +15,27 @@ $( "#ncrpgithubbutton" ).click(function() {
 
   window.open(buttonlink)
 });
+
+$( "#linkedin" ).attr('data-buttonlink', 'https://www.linkedin.com/in/kw0225/')
+
+$( "#linkedin" ).click(function() {
+  const buttonlink = $("#linkedin").data('buttonlink')
+
+  window.open(buttonlink)
+});
+
+$( "#github" ).attr('data-buttonlink', 'https://github.com/kylewhite0225')
+
+$( "#github" ).click(function() {
+  const buttonlink = $("#github").data('buttonlink')
+
+  window.open(buttonlink)
+});
+
+$( "#resume" ).attr('data-buttonlink', 'resume.html')
+
+$( "#resume" ).click(function() {
+  const buttonlink = $("#resume").data('buttonlink')
+
+  window.open(buttonlink)
+});
